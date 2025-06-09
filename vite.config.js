59c3bbcda6d275196e95909fc99fc065c5357bf5
Anh Tu Nguyen <106453722+leo2971998@users.noreply.github.com>
2025-06-09 01:46:14 -0500

    Fixed the deployment issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // This should match your repository name
+  base: '/portfolio/',
   build: {
     outDir: 'dist'
   }
